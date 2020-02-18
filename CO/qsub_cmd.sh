e8e83dfc23f7b8eba3886154bcb1594f9f280646
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2020-02-19 01:59:41 +0900

    Cosmetic changes

--- CO/qsub_cmd.sh
+++ CO/qsub_cmd.sh
@@ -2,16 +2,15 @@
 #$ -cwd
 #$ -pe fillup 6
 #$ -N CO
-
-#disable OPENMP parallelism
+#
 setenv OMP_NUM_THREADS 1
-
-# execuable of the STATE code
+#
+# Execuable of the STATE code
 ln -fs ${HOME}/STATE/src/state-5.6.6_beta/src/STATE .
-
-# pseudopotential data
+#
+# Pseudopotential data
 ln -fs ${HOME}/STATE/gncpp/pot.C_pbe1 fort.37
 ln -fs ${HOME}/STATE/gncpp/pot.O_pbe1 fort.38
- 
-# launch STATE
+#
+# Run!
 mpirun -np $NSLOTS ./STATE < nfinp_1 > nfout_1
