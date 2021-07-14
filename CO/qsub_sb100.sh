8f0d20a3cd7e881c82e17948d75439ed67eff1f3
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2021-07-14 12:38:49 +0900

    job script for sb100 is newly added

---
+++ CO/qsub_sb100.sh
@@ -0,0 +1,32 @@
+#$ -S /bin/sh
+#$ -cwd
+#$ -q sb.q
+#$ -pe x6 6
+#$ -N CO
+
+module load intel/2021.2.0
+module load intelmpi/2021.2.0
+ 
+# Disable the openMP parallelism
+ 
+export OMP_NUM_THREADS=1
+
+unset I_MPI_TCP_NETMASK
+ 
+# Set the execuable of the STATE code
+
+ln -fs ${HOME}/STATE/src/state-5.6.9/src/STATE .
+ 
+# Set the pseudopotential data
+
+ln -fs ../gncpp/pot.C_pbe1
+ln -fs ../gncpp/pot.O_pbe1
+ 
+# Set the input/output file
+
+INPUT_FILE=nfinp_scf
+OUTPUT_FILE=nfout_scf
+
+# Run!
+
+mpirun ./STATE < ${INPUT_FILE} > ${OUTPUT_FILE}
