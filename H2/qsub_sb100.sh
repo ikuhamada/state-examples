782645c05b679c71cdf41e051fbf291aae293a0e
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2021-07-29 12:25:52 +0900

    Added for sb100

---
+++ H2/qsub_sb100.sh
@@ -0,0 +1,32 @@
+#$ -S /bin/sh
+#$ -cwd
+#$ -q sb.q
+#$ -pe x6 6
+#$ -N H2
+
+module load intel/2021.2.0
+module load intelmpi/2021.2.0
+
+# Disable OPENMP parallelism
+
+export OMP_NUM_THREADS=1
+
+unset I_MPI_TCP_NETMASK
+
+# Set the execuable of the STATE code
+
+ln -fs ${HOME}/STATE/src/state-5.6.9/src/STATE .
+
+# Set the pseudopotential data
+
+ln -fs ../gncpp/pot.H_pbe1_sp_new
+ 
+# Set the input/output file
+
+INPUT_FILE=nfinp_qmd
+OUTPUT_FILE=nfout_qmd
+
+# Run!
+
+mpirun ./STATE < ${INPUT_FILE} > ${OUTPUT_FILE}
+
