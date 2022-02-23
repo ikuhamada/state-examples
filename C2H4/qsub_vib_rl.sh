98e14e5761c25d344f8c0490baeb78521b98ca6b
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2022-02-23 16:48:40 +0900

    Update for rl

--- C2H4/qsub_vib_rl.sh
+++ C2H4/qsub_vib_rl.sh
@@ -2,26 +2,27 @@
 #$ -cwd
 #$ -q rl.q
 #$ -pe x6 6
-#$ -N CO
- 
-# Disable the openMP parallelism
- 
+#$ -N C2H4
+
+# Disable OPENMP parallelism
+
 export OMP_NUM_THREADS=1
- 
+
 # Set the executable of the STATE code
 
 ln -fs ${HOME}/STATE/src/state/src/STATE .
- 
+
 # Set the pseudopotential data
 
-ln -fs ../gncpp/pot.C_pbe1
-ln -fs ../gncpp/pot.O_pbe1
- 
+ln -fs ../gncpp/pot.C_pbe3
+ln -fs ../gncpp/pot.H_lda3
+
 # Set the input/output file
 
-INPUT_FILE=nfinp_scf
-OUTPUT_FILE=nfout_scf
-
+INPUT_FILE=nfinp_vib
+OUTPUT_FILE=nfout_vib
+ 
 # Run!
 
 mpirun -np $NSLOTS ./STATE < ${INPUT_FILE} > ${OUTPUT_FILE}
+
